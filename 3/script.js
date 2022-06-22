7e7b1f2652d4d8d08efa8643f437344517530d12
Sergii Anikin <sergeant.ua@gmail.com>
2022-06-22 04:19:03 +0300

    size fix

--- 3/script.js
+++ 3/script.js
@@ -2,22 +2,15 @@ const mainField = document.getElementById('main-field');
 const mainElements = mainField.childNodes;
 const arrows = document.getElementById('arrows');
 const settings = document.forms['settings'];
+// const inputHeight = document.getElementById('height');
+// const inputWidth = document.getElementById('width');
+// const inputObs = document.getElementById('obstacles');
 
 let active = {};
 let cells = [];
 let fieldHeight = 4;
 let fieldWidth = 6;
 let obstacles = 4;
-
-settings.onsubmit = e => {
-  e.preventDefault();
-  let data = new FormData(settings);
-  fieldHeight = +data.get('height');
-  fieldWidth = +data.get('width');
-  obstacles = +data.get('obstacles');
-  if (obstacles > fieldWidth * fieldHeight / 2) obstacles = Math.floor(fieldWidth * fieldHeight / 2);
-  setCells();
-}
 
 const getCells = () => {
   for (let row = 0; row < fieldHeight; row++) {
@@ -56,7 +49,7 @@ const setCells = () => {
   getCells();
   mainField.style.gridTemplate = `repeat(${fieldHeight}, 1fr) / repeat(${fieldWidth}, 1fr)`;
   mainField.innerHTML = Array(fieldHeight * fieldWidth).fill('<div></div>').join('');
-  mainElements.forEach(elem => elem.style.height = `${elem.offsetWidth}px`);
+  mainField.style.height = `${mainField.offsetWidth/fieldWidth*fieldHeight}px`;
   setCellsStyles();
 }
 setCells();
@@ -104,3 +97,24 @@ document.addEventListener('keydown', e => {
     setCellsStyles();
   }
 })
+
+window.addEventListener('resize', () => {
+  mainField.style.height = `${mainField.offsetWidth/fieldWidth*fieldHeight}px`;
+});
+
+settings.onsubmit = e => {
+  e.preventDefault();
+  let data = new FormData(settings);
+  fieldHeight = +data.get('height');
+  fieldWidth = +data.get('width');
+  obstacles = +data.get('obstacles');
+  if (obstacles > fieldWidth * fieldHeight / 2) {
+    obstacles = Math.floor(fieldWidth * fieldHeight / 2);
+    settings[0].value = obstacles;
+  }
+  setCells();
+}
+
+settings.addEventListener('change', (e) => {
+  // set inputs min max settings
+});
